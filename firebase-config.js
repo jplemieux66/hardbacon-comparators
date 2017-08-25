efbf4a749e65ab3de5a0e0dd1171b8940cf9acef
JP <phillemieux66@hotmail.com>
2017-08-25 17:42:29 -0400

    Add firebase config

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+var config = {
+  apiKey: "AIzaSyCwJOoJc-bTBsmCGQaX1349mLRe2xuAmlA",
+  authDomain: "hardbacon-comparators-demo.firebaseapp.com",
+  databaseURL: "https://hardbacon-comparators-demo.firebaseio.com",
+  projectId: "hardbacon-comparators-demo",
+  storageBucket: "hardbacon-comparators-demo.appspot.com",
+  messagingSenderId: "1531169764"
+};
+
+var getConfig = () => {
+  return config;
+}
+
+module.exports = {
+  getConfig
+}
